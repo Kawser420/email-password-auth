3923f4efec3aab0d3c0bd644e05a00516250d9aa
Kawser420 <muhammadkawser202@gmail.com>
2023-10-30 14:31:38 +0600

    make sure file upload continuing

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -1,13 +1,70 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import app from '../../firebase/firebase.config';
+import { Link } from 'react-router-dom';
+
+const auth = getAuth(app);
+
 
 const Login = () => {
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+    
+    const emailRef = useRef();
+
+
     const handleLogin = event => {
         event.preventDefault();
+
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
         console.log(email, password);
+
+        //Validation
+        setError('');
+        setSuccess('');
+
+        if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+            setError('please at least tow characters uppercase. ex: AB')
+            return
+        }
+        else if (!/(?=.*[!@#$&*])/.test(password)) {
+            setError('Please Enter  "?=.*[!@#$&*" this type characters')
+            return
+        }
+
+        else if (password.length < 6) {
+            setError('Please inter 6 characters latter')
+            return
+        }
+
+        signInWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser)
+                setSuccess('Login successfully');
+                setError('');
+            })
+            .catch(error => {
+                console.log(error.message);
+            })
     }
+
+    const handleResetPassword = event =>{
+        const email = emailRef.current.value;
+        if(!email) {
+            alert('please provide the email Address to reset password')
+        }
+        sendPasswordResetEmail(auth, email)
+        .then( () => {
+            alert('please check your Email')
+        })
+        .catch(error => {
+            console.log(error.message)
+        })
+    }
+
     return (
         <div>
             <div className="container">
@@ -23,6 +80,7 @@ const Login = () => {
                                     className="form-control w-50 rounded mb-4"
                                     id="username"
                                     placeholder="Enter your username"
+                                    ref={emailRef}
                                     required
                                 />
                             </div>
@@ -41,6 +99,10 @@ const Login = () => {
                                 Login
                             </button>
                         </form>
+                        <p>Forget Your Password? Please, <button onClick={handleResetPassword} className='btn btn-link'>Reset Password</button></p>
+                        <p><small>are you new to website? <Link to="/resister">Resister</Link></small></p>
+                        <p className='text-danger'>{error}</p>
+                        <p className='text-success'>{success}</p>
                         <label className="form-check-label" htmlFor="termsCheckbox">
                             ☑ I agree to the terms and conditions
                         </label>
--- src/component/Resister/Resister.jsx
+++ src/component/Resister/Resister.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, updateProfile } from "firebase/auth";
 import app from '../../firebase/firebase.config';
+import { Link } from 'react-router-dom';
 
 const auth = getAuth(app)
 
@@ -16,7 +17,8 @@ const Resister = () => {
         //2.collect form data
         const email = event.target.email.value;
         const password = event.target.password.value;
-        console.log(email, password)
+        const name = event.target.name.value;
+        console.log(email, password, name)
         //validate password 
         if (!/(?=.*[A-Z])/.test(password)) {
             setError('please add at least one Uppercase')
@@ -41,12 +43,35 @@ const Resister = () => {
                 setError('');
                 event.target.reset();
                 setSuccess('User has been created successfully')
+                sendVerifiedEmail(result.user);
+                updateUserData(result.user, name)
             })
             .catch(error => {
                 console.log(error.message);
                 setError(error.message);
             })
 
+    }
+
+    // send email verified
+    const sendVerifiedEmail = (user) => {
+        sendEmailVerification(user)
+        .then(result => {
+            console.log(result)
+            alert("Please verified your email adders. Check Email")
+        })
+    }
+
+    const updateUserData = (user, name) => {
+        updateProfile(user, {
+            displayName: user
+        })
+        .then( () => {
+            console.log('user update name');
+        })
+        .catch(error => {
+            console.log(error.message)
+        })
     }
 
     const handleEmailChange = (event) => {
@@ -57,16 +82,20 @@ const Resister = () => {
     const handlePasswordBlur = (event) => {
         // console.log(event.target.value);
     }
+
     return (
         <div className='w-50 mx-auto'>
             <h2>Please Resister</h2>
             <form onSubmit={handleSubmit}>
+                < input className='w-50 rounded mb-4 pt-2 pb-2' type="name" name="name" id="" placeholder='Your Name' required />
+                <br />
                 < input className='w-50 rounded mb-4 pt-2 pb-2' type="email" onChange={handleEmailChange} name="email" id="" placeholder='Your Email' required />
                 <br />
                 < input className='w-50 rounded mb-4 pt-2 pb-2' type="password" onBlur={handlePasswordBlur} name="password" id="" placeholder='Your Password' required />
                 <br />
                 <button className='btn btn-primary' type="submit">Resister Now</button>
             </form>
+            <p><small>Already have an Account? Please <Link to="/login">Login</Link></small></p>
             <p className='text-danger'>{error}</p>
             <p className='text-success'>{success}</p>
         </div>
